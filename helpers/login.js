13a924d0751da1c0f88325ad8c123fd93784fe75
jose sepulveda <jose.sepulveda@cnr.gob.cl>
2024-05-31 12:05:22 -0400

    se agregan funcionalidades al login

--- helpers/login.js
+++ helpers/login.js
@@ -5,6 +5,7 @@ function showHideCards(){
 
     const dv_login = document.getElementById('dv_login');
     const dv_register = document.getElementById('dv_register');
+    const link_registro = document.getElementById('link_registro');
 
     item_login.addEventListener('click', () => {
 
@@ -23,6 +24,25 @@ function showHideCards(){
         dv_login.classList.add('d-none');
         dv_register.classList.remove('d-none');
     });
+
+    link_registro.addEventListener('click', () => {
+        
+        item_register.classList.add('active');
+        item_login.classList.remove('active');
+
+        dv_login.classList.add('d-none');
+        dv_register.classList.remove('d-none');
+    });
+}
+
+function recuperarContrasenia(){
+
+    const link_contrasenia = document.getElementById('link_contrasenia');
+    link_contrasenia.addEventListener('click', () => {
+        var myModal = document.getElementById('modalRecuperarContrasenia');
+        var modal = new bootstrap.Modal(myModal);
+        modal.show();
+    })
 }
 
 function login(){
@@ -33,8 +53,8 @@ function login(){
 }
 
 
-
 document.addEventListener('DOMContentLoaded', (event) => {
     showHideCards();
     login();
+    recuperarContrasenia();
 });
